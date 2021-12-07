a371e805ff2364992ac1215428bdf7a2d9d2c1a8
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2021-12-07 10:04:29 +0530

    Adding daily  batch of work.

--- Java Projects/java-nimbus/src/main/java/com/kchandrakant/learning/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.kchandrakant.learning;
-
-public class AuthenticationException extends RuntimeException {
-
-    public AuthenticationException(String errorMessage) {
-        super(errorMessage);
-    }
-
-}
